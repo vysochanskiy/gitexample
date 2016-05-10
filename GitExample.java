8e1ecc62d476837aedb61298e1a3abf7c571d764
Vladimir Vysochanskiy <v.vysochanskiy@yandex.ru>
2016-05-10 14:08:35 +0300

    New second string.

--- GitExample.java
+++ GitExample.java
@@ -3,7 +3,7 @@ public class GitExample {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("Second string for a test.");
+		System.out.println("New second string for a test.");
 	}
 
 }
