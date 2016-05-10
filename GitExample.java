5291faccd8358ab7872dec1c4b1e13c230c22af4
Vladimir Vysochanskiy <v.vysochanskiy@yandex.ru>
2016-05-10 14:05:47 +0300

    Add a new string to another branch.

--- GitExample.java
+++ GitExample.java
@@ -4,6 +4,8 @@ public class GitExample {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Second string for a test.");
+		int someInteger = 0; // debug line example.
+		System.out.println("Third string in another branch.");
 	}
 
 }
